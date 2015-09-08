b08845e90238d702297d7f7f5d7ed977f015fea4
Jeong YS <dionicis1@gmail.com>
2015-09-08 17:59:57 +0900

    되나

---
+++ Kkako/Assets/Script/Move.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	public float speed;
+	public static Move instance;
+
+	void Awake(){
+		instance = this;
+	}
+
+	void FixedUpdate(){
+		gameObject.GetComponent<Transform>().Translate(Vector3.right*Time.deltaTime*speed);
+	}
+
+}
---
+++ Kkako/Assets/Script/TimeSleep.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class TimeSleep : MonoBehaviour {
+
+	public void SlowMotion(){
+		StartCoroutine(WaitTime());
+	}
+	
+	IEnumerator WaitTime() {
+		Move.instance.speed = 0.05f;
+		yield return new WaitForSeconds(2.0f);
+		Move.instance.speed = 0.2f;
+	}
+}
