82459a005c1e553bba91568e4f6496c566afa882
Jeong YS <dionicis1@gmail.com>
2015-11-16 20:39:48 +0900

    리펙토링 완료(안된건 나도 몰라서 안함)

--- Kkako/Assets/Script/CameraFollow.cs
+++ Kkako/Assets/Script/CameraFollow.cs
@@ -5,15 +5,22 @@ public class CameraFollow : MonoBehaviour {
 
 	private Transform tr;
 	public Transform target;
+	public float size;
 
 	// Use this for initialization
 	void Start () {
-		tr = this.GetComponent<Transform>();
+		tr = this.GetComponent<Transform>(); //카메라의 위치값.
+		target = target.GetComponent<Transform>(); //카메라가 따라갈 케릭터의 위치값
+		size = GetComponent<Camera>().orthographicSize; //카메라의 사이즈
 
-		target = target.GetComponent<Transform>();
+
 	}
 
 	void Update () {
-		tr.position = new Vector3(target.position.x,target.position.y,-0.01f);
+		tr.position = new Vector3(target.position.x,target.position.y,-0.01f); //카메라가 캐릭터를 따라간다.
+	}
+
+	public void sizeUpdate(){ //스크롤 바를 움직였을때 카메라 사이즈를 바꾼다.
+		Camera.main.orthographicSize = UIProgressBar.current.value*2;
 	}
 }
--- Kkako/Assets/Script/Move.cs
+++ Kkako/Assets/Script/Move.cs
@@ -8,25 +8,40 @@ public class Move : MonoBehaviour {
 	public static Move charMove;
 
 	private Transform tr;
-
+	private Ray ray;
+	private RaycastHit hit;
 	private Animator anim;
 
 	void Awake(){
 		charMove = this;
 		tr = this.gameObject.GetComponent<Transform>();
-
-		anim = GetComponent<Animator>();
+		anim = GetComponentInChildren<Animator>();
 	}
 
 	void Update(){
-//		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//		Debug.DrawRay(ray.origin, ray.direction*1000.0f,Color.green);
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Debug.DrawRay(ray.origin,ray.direction*100.0f,Color.green);
 
 //		Vector2 worldSpace = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
 //		Debug.Log (tr.localPosition);
 
-		gameObject.GetComponent<Transform>().Translate(Vector3.right*Time.deltaTime*speed);
+		gameObject.GetComponent<Transform>().Translate(Vector2.right*Time.deltaTime*speed);
+
+//		if(Input.GetMouseButton(0))
+//		{
+//			Debug.Log("mouseDown");
+//			if(UIw)//케릭터를 클릭했을때
+//			{
+//				Debug.Log("Click");
+//				if (Physics2D.Raycast(transform.position, Vector2.down, 0.1f, 1 << 8))
+//				{
+//					Debug.Log("Jump");
+//					GetComponent<Rigidbody2D>().AddForce(Vector2.up*700.0f);
+//				}
+//			}
+//		}
+
 
 //		if(Input.GetMouseButtonDown(0))
 //		{
@@ -39,5 +54,9 @@ public class Move : MonoBehaviour {
 //
 //			}
 //		}
+	}
+
+	public void moveUpdate(){
+		speed = UIScrollBar.current.value;
 	}
 }
